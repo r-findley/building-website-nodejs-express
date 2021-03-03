a4871087bae2b8a747108193f702f19f80f475c5
Ryan Findley <ryanfindley1013@gmail.com>
2021-03-03 09:20:13 -0600

    added routes, data, service, cookies

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
+
+const routes = require('./routes');
 
 const app = express();
 
 const port = 3500;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['gdjipijjweefhi;hdki9433', '93jj;kaljh!jdkljkasjdiof'],
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Your express server is running on ${port}`);
